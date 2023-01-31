afb1ef1df2bd3fbadcdce3fbd3f56e4148bb970c
strubloid <strubloid@gmail.com>
2023-01-31 13:22:34 +0000

    adding new config for npm run live

---
+++ bash/live.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+MY_DIR=$(dirname $(readlink -f "$0") | sed  "s/\/[^\/]*$//")
+USER=$(whoami)
+
+function getOperationalSystem() {
+  case "$OSTYPE" in
+  win*) echo "windows" ;;
+  msys*) echo "windows" ;;
+  *) echo "linux" ;;
+  esac
+}
+
+OS=$(getOperationalSystem)
+NODE_MODULES_DIR="$MY_DIR/node_modules"
+
+echo "==============================================================================="
+echo "=== Deployment ================================================================"
+echo "=== SRC: $MY_DIR"
+echo "=== OS: $OS"
+echo "=== USER: $USER"
+echo "=== Node Modules: $NODE_MODULES_DIR"
+echo "==============================================================================="
+
+
+## Clean of the node_modules
+echo "=== node_modules"
+if [ -d "$NODE_MODULES_DIR" ];
+then
+  echo "=== node_modules : clean"
+  rm -rf "$NODE_MODULES_DIR/*"
+fi
+
+echo "=== node_modules : install"
+npm i
+
+
+echo "=== Node : Serve"
+ng serve
