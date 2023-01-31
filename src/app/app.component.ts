9d10fbf792ed6460483991c68289f00746fa96c4
strubloid <strubloid@gmail.com>
2023-01-31 13:00:46 +0000

    adding new things to check if packages will still work after the webpack version of the angular being build

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import * as moment from 'moment';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'WebpackAngular';
+export class AppComponent implements OnInit {
+    public title = 'WebpackAngular';
+    public currentTimeFRLocale: string | undefined;
+
+    ngOnInit(): void {
+        moment.locale('fr');
+        this.currentTimeFRLocale = moment().format('LLL');
+    }
 }
